c30e4ab53b70ad8762408163a87dffa980691f71
Ashlen <eurydice@riseup.net>
2020-08-31 03:07:18 -0600

    proper status bar spacing and window redirection to workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,15 +11,21 @@ myBorderWidth = 0
 
 myManageHook = composeAll
   [ className =? "Firefox" --> doShift "3:ff"
-  , className =? "Firefox" --> doFloat ]
+  , className =? "Firefox" --> doFloat
+  , className =? "Tor Browser" --> doShift "4:tb"
+  , className =? "Tor Browser" --> doFloat
+  , className =? "mpv" --> doShift "9:mpv"
+  , manageDocks ]
+
 
 myWorkSpaces = ["1:dev","2","3:ff","4:tb","5:pw","6","7","8","9:mpv"]
 
 main = do
-  xmonad $ def
-    { borderWidth       = myBorderWidth
+  xmonad $ docks def
+    { layoutHook        = avoidStruts $ layoutHook def
+    , borderWidth       = myBorderWidth
     , workspaces        = myWorkSpaces
     , modMask           = myModMask
     , terminal          = myTerminal
     , focusFollowsMouse = False
-    , manageHook        = myManageHook }
+    , manageHook        = myManageHook <+> manageHook def }
