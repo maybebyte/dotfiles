1f33a77824ff3435727044aeec81ed7ba1d19db6
Ashlen <eurydice@riseup.net>
2020-09-01 14:48:05 -0600

    myLayoutHook added, added gaps

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Layout.Spacing
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -14,15 +15,19 @@ myManageHook = composeAll
   , className =? "Firefox" --> doFloat
   , className =? "Tor Browser" --> doShift "4:tb"
   , className =? "Tor Browser" --> doFloat
+  , className =? "KeePassXC" --> doShift "5:pw"
   , className =? "mpv" --> doShift "9:mpv"
   , manageDocks ]
 
+myLayoutHook = avoidStruts
+  $ spacingRaw False (Border 20 0 20 0) True (Border 0 20 0 20) True
+  $ Tall 1 (3/100) (1/2) ||| Full
 
 myWorkSpaces = ["1:dev","2","3:ff","4:tb","5:pw","6","7","8","9:mpv"]
 
 main = do
   xmonad $ docks def
-    { layoutHook        = avoidStruts $ layoutHook def
+    { layoutHook        = myLayoutHook
     , borderWidth       = myBorderWidth
     , workspaces        = myWorkSpaces
     , modMask           = myModMask
