62c6156b418ea5d0747bd6ec4ccd073a5110a9d4
Ashlen <eurydice@riseup.net>
2020-09-01 14:55:26 -0600

    KISS

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,25 +11,22 @@ myTerminal = "st"
 myBorderWidth = 0
 
 myManageHook = composeAll
-  [ className =? "Firefox" --> doShift "3:ff"
+  [ className =? "Firefox" --> doShift "3"
   , className =? "Firefox" --> doFloat
-  , className =? "Tor Browser" --> doShift "4:tb"
+  , className =? "Tor Browser" --> doShift "4"
   , className =? "Tor Browser" --> doFloat
-  , className =? "KeePassXC" --> doShift "5:pw"
-  , className =? "mpv" --> doShift "9:mpv"
+  , className =? "KeePassXC" --> doShift "5"
+  , className =? "mpv" --> doShift "9"
   , manageDocks ]
 
 myLayoutHook = avoidStruts
   $ spacingRaw False (Border 20 0 20 0) True (Border 0 20 0 20) True
   $ Tall 1 (3/100) (1/2) ||| Full
 
-myWorkSpaces = ["1:dev","2","3:ff","4:tb","5:pw","6","7","8","9:mpv"]
-
 main = do
   xmonad $ docks def
     { layoutHook        = myLayoutHook
     , borderWidth       = myBorderWidth
-    , workspaces        = myWorkSpaces
     , modMask           = myModMask
     , terminal          = myTerminal
     , focusFollowsMouse = False
