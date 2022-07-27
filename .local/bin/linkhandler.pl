ad5fe7d41b5aaaaf42f0a578830f6465b5807492
Ashlen <eurydice@riseup.net>
2022-07-26 23:35:51 -0600

    restructure into subroutines

--- .local/bin/linkhandler.pl
+++ .local/bin/linkhandler.pl
@@ -43,6 +43,85 @@ use URI;
 
 my $program_name = fileparse $0;
 
+
+sub get_duration {
+	my $yt_url = shift // die "get_duration needs a URL.\n";
+	open my $yt_dlp_fh, '-|', 'yt-dlp', '--get-duration', '--', $yt_url
+		or die "Could not run yt-dlp: $!\n";
+
+	chomp(my $duration = <$yt_dlp_fh> // die "No duration.\n");
+
+	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
+	die "yt-dlp: non-zero exit of $?" if $?;
+
+	return $duration;
+}
+
+sub get_title {
+	my $yt_url = shift // die "get_title needs a URL.\n";
+	open my $yt_dlp_fh, '-|', 'yt-dlp', '-e', '--', $yt_url
+		or die "Could not run yt-dlp: $!\n";
+
+	chomp(my $title = <$yt_dlp_fh> // die "No title.\n");
+
+	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
+	die "yt-dlp: non-zero exit of $?" if $?;
+
+	return $title;
+}
+
+sub copy_to_clipboard {
+	chomp(my $stuff_to_copy = shift // die "copy_to_clipboard needs something to copy.\n");
+
+	open my $xclip_fh, '|-', 'xclip', '-selection', 'clipboard'
+		or die "Could not send duration to xclip: $!\n";
+
+	print $xclip_fh $stuff_to_copy;
+
+	close $xclip_fh or die "Could not close xclip filehandle: $!\n";
+	die "xclip: non-zero exit of $?" if $?;
+
+	return length($stuff_to_copy);
+}
+
+sub fetch_response {
+	my $fetch_url = shift // die "fetch_response needs a URL.\n";
+	my $http = HTTP::Tiny->new(
+		verify_SSL => 1,
+	);
+	die "TLS is unsupported: $!\n" unless $http->can_ssl;
+
+	my $response = $http->get($fetch_url);
+	die "$response->{status} $response->{reason}" unless $response->{success};
+
+	return $response->{content};
+}
+
+sub check_dependencies {
+	# Is there a portable way to do this that doesn't involve shell?
+	open my $sh_fh, '-|', '/bin/sh', '-c', "command -v @_";
+
+	my @dependency_checks;
+	while (<$sh_fh>) {
+		chomp;
+		push @_, $_;
+	}
+
+	if ($#_ != $#dependency_checks) {
+		my $count = 0;
+		while (<@_>) {
+			$dependency_checks[$count] // '' =~ /$_/
+				or die "$_ is not installed!\n";
+			$count++;
+		}
+	}
+
+	close $sh_fh or die "Could not close shell filehandle: $!\n";
+
+	return $#dependency_checks;
+}
+
+
 my @dependencies = (
 	'fzf',
 	'mpv',
@@ -72,28 +151,9 @@ chomp(my $url = $ARGV[0] // <STDIN> // die "$program_name needs a URL!\n");
 
 $url = URI->new($url);
 die "$program_name only handles the 'https' scheme.\n"
-	unless $url->scheme eq 'https';
+	unless $url->scheme // '' eq 'https';
 
-
-# Is there a portable way to do this that doesn't involve shell?
-open my $sh_fh, '-|', '/bin/sh', '-c', "command -v @dependencies";
-
-my @dependency_checks;
-while (<$sh_fh>) {
-	chomp;
-	push @dependency_checks, $_;
-}
-
-if ($#dependencies != $#dependency_checks) {
-	my $count = 0;
-	while (<@dependencies>) {
-		$dependency_checks[$count] // '' =~ /$_/
-			or die "$_ is not installed!\n";
-		$count++;
-	}
-}
-
-close $sh_fh or die "Could not close shell filehandle: $!\n";
+check_dependencies @dependencies;
 
 
 my $fzf_pid = open2(my $fzf_out, my $fzf_in, 'fzf')
@@ -108,46 +168,16 @@ waitpid $fzf_pid, 0;
 
 
 if ($option eq 'copy duration') {
-	open my $yt_dlp_fh, '-|', 'yt-dlp', '--get-duration', '--', $url
-		or die "Could not run yt-dlp: $!\n";
-
-	chomp(my $duration = <$yt_dlp_fh> // die "No duration.\n");
-
-	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
-	die "yt-dlp: non-zero exit of $?" if $?;
-
-
-	open my $xclip_fh, '|-', 'xclip', '-selection', 'clipboard'
-		or die "Could not send duration to xclip: $!\n";
-
-	print $xclip_fh $duration;
-
-	close $xclip_fh or die "Could not close xclip filehandle: $!\n";
-	die "xclip: non-zero exit of $?" if $?;
-
+	my $duration = get_duration $url;
+	copy_to_clipboard $duration;
 
 	exec 'notify-send', '--', "Copied video duration: ($duration).";
 	die "exec 'notify-send' failed: $!\n";
 }
 
 elsif ($option eq 'copy title') {
-	open my $yt_dlp_fh, '-|', 'yt-dlp', '-e', '--', $url
-		or die "Could not run yt-dlp: $!\n";
-
-	chomp(my $title = <$yt_dlp_fh> // die "No title.\n");
-
-	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
-	die "yt-dlp: non-zero exit of $?" if $?;
-
-
-	open my $xclip_fh, '|-', 'xclip', '-selection', 'clipboard'
-		or die "Could not send duration to xclip: $!\n";
-
-	print $xclip_fh $title;
-
-	close $xclip_fh or die "Could not close xclip filehandle: $!\n";
-	die "xclip: non-zero exit of $?" if $?;
-
+	my $title = get_title $url;
+	copy_to_clipboard $title;
 
 	exec 'notify-send', '--', "Copied video title: ($title).";
 	die "exec 'notify-send' failed: $!\n";
@@ -161,16 +191,10 @@ elsif ($option eq 'download audio') {
 elsif ($option eq 'download file') {
 	chdir or die "Could not change directory to HOME: $!\n";
 
-	my $http = HTTP::Tiny->new(
-		verify_SSL => 1,
-	);
-	die "TLS is unsupported: $!\n" unless $http->can_ssl;
-
-	my $response = $http->get($url);
-	die "$response->{status} $response->{reason}" unless $response->{success};
-
+	my $content = fetch_response($url);
 	my @uri_segments = $url->path_segments;
 	my $host = $url->authority;
+
 
 	my $dir_to_write_to = File::Spec->catdir('Downloads', $host);
 
@@ -182,11 +206,11 @@ elsif ($option eq 'download file') {
 	chdir $dir_to_write_to
 		or die "Could not change directory to '$dir_to_write_to': $!\n";
 
+
 	open my $download_fh, '>', $uri_segments[-1]
 		or die "Could not open file named '$uri_segments[-1]': $!\n";
 
-	say $download_fh $response->{content};
-
+	say $download_fh $content;
 	close $download_fh or die "Could not close filehandle: $!\n";
 }
 
@@ -203,44 +227,24 @@ elsif ($option eq 'listen') {
 }
 
 elsif ($option eq 'print duration') {
-	open my $yt_dlp_fh, '-|', 'yt-dlp', '--get-duration', '--', $url
-		or die "Could not run yt-dlp: $!\n";
-
-	chomp(my $duration = <$yt_dlp_fh> // die "Nonexistent duration.\n");
-
-	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
-	die "yt-dlp non-zero exit of $?" if $?;
+	my $duration = get_duration $url;
 
 	exec 'notify-send', '--', "Video duration: ($duration).";
 	die "exec 'notify-send' failed: $!\n";
 }
 
 elsif ($option eq 'print title') {
-	open my $yt_dlp_fh, '-|', 'yt-dlp', '-e', '--', $url
-		or die "Could not run yt-dlp: $!\n";
-
-	chomp(my $title = <$yt_dlp_fh> // die "No title.\n");
-
-	close $yt_dlp_fh or die "Could not close yt-dlp filehandle: $!\n";
-	die "yt-dlp: non-zero exit of $?" if $?;
+	my $title = get_title $url;
 
 	exec 'notify-send', '--', "Video title: ($title).";
 	die "exec 'notify-send' failed: $!\n";
 }
 
 elsif ($option eq 'read') {
-	my $http = HTTP::Tiny->new(
-		verify_SSL => 1,
-	);
-	die "TLS is unsupported: $!\n" unless $http->can_ssl;
-
-	my $response = $http->get($url);
-	die "$response->{status} $response->{reason}" unless $response->{success};
-
 	open my $zathura_fh, '|-', 'zathura', '-'
 		or die "Could not run zathura: $!\n";
 
-	say $zathura_fh $response->{content};
+	say $zathura_fh fetch_response($url);
 
 	close $zathura_fh or die "Could not close zathura filehandle: $!\n";
 	die "zathura: non-zero exit of $?" if $?;
