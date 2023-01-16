1a7df885b1a7bbfe4297c091c8f07b4333c4d081
Ashlen <eurydice@riseup.net>
2023-01-15 17:14:48 -0700

    this should have been a list

--- .config/nvim/lua/vim_g.lua
+++ .config/nvim/lua/vim_g.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ','
 -- https://github.com/jamessan/vim-gnupg/issues/119
 -- https://github.com/jamessan/vim-gnupg/issues/32
 vim.g.GPGUsePipes = 1
-vim.g.GPGDefaultRecipients = "90965AE120F8E848979DEA4853670DEBCF375780"
+vim.g.GPGDefaultRecipients = { "90965AE120F8E848979DEA4853670DEBCF375780" }
 
 -- VimTeX should use zathura as the viewer
 vim.g.vimtex_view_method = 'zathura'
