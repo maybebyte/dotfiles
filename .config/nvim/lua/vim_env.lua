d5c08d3f80bd8b8ed721e1340a3f0a4025feb06c
Ashlen <eurydice@riseup.net>
2023-01-27 02:20:01 -0700

    fix vim environment vars here
    (wouldn't populate when vars were set to blank)

--- .config/nvim/lua/vim_env.lua
+++ .config/nvim/lua/vim_env.lua
@@ -3,13 +3,13 @@
 -- Define variables so that even if they're undefined by a specific user, neovim
 -- will know what to do with them.
 vim.env.XDG_CONFIG_HOME =
-	os.getenv("XDG_CONFIG_HOME") or
-	os.getenv("HOME") .. '/.local/config'
+	vim.env.XDG_CONFIG_HOME or
+	os.getenv('HOME') .. '/.local/config'
 
 vim.env.XDG_DATA_HOME =
-	os.getenv("XDG_DATA_HOME") or
-	os.getenv("HOME") .. '/.local/share'
+	vim.env.XDG_DATA_HOME or
+	os.getenv('HOME') .. '/.local/share'
 
 vim.env.XDG_STATE_HOME =
-	os.getenv("XDG_STATE_HOME") or
-	os.getenv("HOME") .. '/.local/state'
+	vim.env.XDG_STATE_HOME or
+	os.getenv('HOME') .. '/.local/state'
