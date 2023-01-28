0912eeb008a78fd1a82a661143981559d6852f46
Ashlen <eurydice@riseup.net>
2023-01-27 17:16:47 -0700

    backup directory defensive coding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,18 @@ require('my.plugins')
 
 local backup_directory = vim.env.XDG_STATE_HOME .. '/nvim/backup'
 
-if not os.rename(backup_directory, backup_directory) then
-	os.execute('mkdir -p ' .. backup_directory)
+-- Make sure the backup directory is present and really a directory.
+if os.rename(backup_directory, backup_directory) then
+	if not os.rename(backup_directory, backup_directory .. '/') then
+		vim.api.nvim_err_writeln(
+			backup_directory .. ' is a file, not a directory.'
+		)
+		vim.opt.backup = false
+	end
+-- If nothing is there, attempt to create it.
+else
+	if not os.execute('mkdir -p ' .. backup_directory) then
+		vim.api.nvim_err_writeln('Failed to create ' .. backup_directory)
+		vim.opt.backup = false
+	end
 end
