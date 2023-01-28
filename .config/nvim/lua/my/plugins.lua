df844f9d5ed41f0fd6119e689e6a264d2b05c039
Ashlen <eurydice@riseup.net>
2023-01-27 17:00:39 -0700

    check for plugin support first

--- .config/nvim/lua/my/plugins.lua
+++ .config/nvim/lua/my/plugins.lua
@@ -1,28 +1,35 @@
 -- luacheck: globals vim
-local Plug = vim.fn['plug#']
 
--- plugins
-vim.call('plug#begin', vim.env.XDG_DATA_HOME .. '/nvim/site/autoload/plugged')
+local plugin_file = vim.env.XDG_DATA_HOME .. '/nvim/site/autoload/plug.vim'
 
-Plug 'junegunn/goyo.vim'
-Plug 'junegunn/vim-easy-align'
-Plug 'scrooloose/nerdcommenter'
-Plug 'scrooloose/nerdtree'
-Plug 'scrooloose/syntastic'
-Plug 'tpope/vim-surround'
+if os.rename(plugin_file, plugin_file) then
+	local Plug = vim.fn['plug#']
 
-if os.getenv('DISPLAY') then
-	Plug 'ap/vim-css-color'
-	Plug 'dylanaraps/wal.vim'
-	Plug 'kovetskiy/sxhkd-vim'
-	Plug 'plasticboy/vim-markdown'
-	Plug 'lervag/vimtex'
-	Plug 'jamessan/vim-gnupg'
-end
+	-- plugins
+	vim.call('plug#begin', vim.env.XDG_DATA_HOME .. '/nvim/site/autoload/plugged')
 
-vim.call('plug#end')
+	Plug 'junegunn/goyo.vim'
+	Plug 'junegunn/vim-easy-align'
+	Plug 'scrooloose/nerdcommenter'
+	Plug 'scrooloose/nerdtree'
+	Plug 'scrooloose/syntastic'
+	Plug 'tpope/vim-surround'
 
--- https://github.com/dylanaraps/pywal
-if os.getenv('DISPLAY') then
-	vim.cmd.colorscheme('wal')
+	if os.getenv('DISPLAY') then
+		Plug 'ap/vim-css-color'
+		Plug 'dylanaraps/wal.vim'
+		Plug 'kovetskiy/sxhkd-vim'
+		Plug 'plasticboy/vim-markdown'
+		Plug 'lervag/vimtex'
+		Plug 'jamessan/vim-gnupg'
+	end
+
+	vim.call('plug#end')
+
+	-- https://github.com/dylanaraps/pywal
+	if os.getenv('DISPLAY') then
+		vim.cmd.colorscheme('wal')
+	end
+else
+	vim.api.nvim_err_writeln(plugin_file .. ' not found. Plugins are disabled.')
 end
