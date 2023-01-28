083cf37746d92869d2aa7bceecee3d3e4506fcc4
Ashlen <eurydice@riseup.net>
2023-01-27 17:44:28 -0700

    convert some more stuff

--- .config/nvim/lua/my/keybindings.lua
+++ .config/nvim/lua/my/keybindings.lua
@@ -32,27 +32,49 @@ vim.keymap.set(
 )
 
 vim.keymap.set(
-	'n', '<leader>w', ':%s/\\s\\+$//e<CR>',
+	'n', '<leader>w',
+	function()
+		vim.cmd('%s/\\s\\+$//e')
+	end,
 	{ desc = 'Delete all trailing whitespace.' }
 )
 
 vim.keymap.set(
-	'n', '<leader>e', ':%s/\\n\\+\\%$//e<CR>',
+	'n', '<leader>e',
+	function()
+		vim.cmd('%s/\\n\\+\\%$//e')
+	end,
 	{ desc = 'Delete all trailing newlines.' }
 )
 
 vim.keymap.set(
-	'n', '<leader>o', ':set spell! spelllang=en_us<CR>',
+	'n', '<leader>o',
+	function()
+		-- There's probably a better way to do this in Lua, but I don't know it
+		-- right now.
+		if vim.o.spell == true then
+			vim.o.spell = false
+		else
+			vim.o.spell = true
+		end
+		vim.opt.spelllang = 'en_us'
+	end,
 	{ desc = "Toggle spell check ('o' for orthography)." }
 )
 
 vim.keymap.set(
-	'n', '<leader>g', ':Goyo<CR>',
+	'n', '<leader>g',
+	function()
+		vim.cmd('Goyo')
+	end,
 	{ desc = 'Toggle Goyo.' }
 )
 
 vim.keymap.set(
-	'n', '<leader>n', ':NERDTreeToggle<CR>',
+	'n', '<leader>n',
+	function()
+		vim.cmd('NERDTreeToggle')
+	end,
 	{ desc = 'Toggle NERD Tree.' }
 )
 
