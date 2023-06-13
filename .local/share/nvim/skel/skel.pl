6d270bef23c920f0a3868457c659af3cf3162a21
Ashlen <dev@anthes.is>
2023-06-13 14:13:39 -0600

    skel.pl: adjust for cross-platform

--- .local/share/nvim/skel/skel.pl
+++ .local/share/nvim/skel/skel.pl
@@ -27,22 +27,28 @@ use warnings qw(FATAL utf8);
 use English;
 use Unicode::Normalize qw(normalize);
 
-if ( $OSNAME eq 'openbsd' ) {
-	use OpenBSD::Pledge;
-	use OpenBSD::Unveil;
-
-	sub pledge_or_die (@syscalls) {
-		pledge @syscalls or die "Pledge failed: $OS_ERROR";
+BEGIN {
+	if ( $OSNAME eq 'openbsd' ) {
+		require OpenBSD::Pledge;
+		require OpenBSD::Unveil;
+		OpenBSD::Pledge->import('pledge');
+		OpenBSD::Unveil->import('unveil');
 	}
-
-	sub unveil_or_die (%file_perms) {
-		while ( my ( $file, $file_perms ) = each %file_perms ) {
-			unveil $file, $file_perms or die "Unveil failed: $OS_ERROR";
-		}
+	else {
+		sub pledge { return 1; }
+		sub unveil { return 1; }
 	}
 }
 
-if ( $OSNAME eq 'openbsd' ) {
-	unveil_or_die map { $ARG, 'r' } @INC;
-	pledge_or_die qw(rpath);
+sub pledge_or_die (@syscalls) {
+	pledge @syscalls or die "Pledge failed: $OS_ERROR";
 }
+
+sub unveil_or_die (%file_perms) {
+	while ( my ( $file, $file_perms ) = each %file_perms ) {
+		unveil $file, $file_perms or die "Unveil failed: $OS_ERROR";
+	}
+}
+
+unveil_or_die map { $ARG, 'r' } @INC;
+pledge_or_die qw(rpath);
