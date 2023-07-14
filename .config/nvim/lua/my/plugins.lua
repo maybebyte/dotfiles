21fbd49e170657af685cda64925c81ffaf901c6e
Ashlen <dev@anthes.is>
2023-07-14 10:55:56 -0600

    Add ChatGPT integration, fix bug with pinentry by requiring X.
    See this for more details:
    https://github.com/jamessan/vim-gnupg/issues/32

--- .config/nvim/lua/my/plugins.lua
+++ .config/nvim/lua/my/plugins.lua
@@ -22,9 +22,18 @@ if os.rename(plugin_file, plugin_file) then
 		Plug 'plasticboy/vim-markdown'
 		Plug 'lervag/vimtex'
 		Plug 'jamessan/vim-gnupg'
+		Plug 'jackMort/ChatGPT.nvim'
+		-- Dependencies of ChatGPT.nvim
+		Plug 'MunifTanjim/nui.nvim'
+		Plug 'nvim-lua/plenary.nvim'
+		Plug('nvim-telescope/telescope.nvim', { tag = '0.1.2'})
 	end
 
 	vim.call('plug#end')
 else
 	vim.api.nvim_err_writeln(plugin_file .. ' not found. Plugins are disabled.')
 end
+
+require("chatgpt").setup({
+    api_key_cmd = "gpg --decrypt " .. vim.fn.expand("$HOME") .. "/passwords/api/chatgpt.txt.gpg"
+})
