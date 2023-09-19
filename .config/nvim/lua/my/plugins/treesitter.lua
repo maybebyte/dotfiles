20134e9ca05b3151f445d597cb2a64bb6a546443
Ashlen <dev@anthes.is>
2023-09-19 14:17:58 -0600

    refactor(treesitter): use opts and config, not init

--- .config/nvim/lua/my/plugins/treesitter.lua
+++ .config/nvim/lua/my/plugins/treesitter.lua
@@ -3,41 +3,42 @@ return {
 	build = function()
 		vim.cmd("TSUpdate")
 	end,
-	init = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"css",
-				"go",
-				--'diff', -- this assumes git, so disable for now
-				"html",
-				"lua",
-				"make",
-				"python",
-				"query",
-				"sxhkdrc",
-				"vim",
-				"vimdoc",
-			},
-			sync_install = false,
-			auto_install = false,
+	opts = {
+		ensure_installed = {
+			"c",
+			"css",
+			"go",
+			--'diff', -- this assumes git, so disable for now
+			"html",
+			"lua",
+			"make",
+			"python",
+			"query",
+			"sxhkdrc",
+			"vim",
+			"vimdoc",
+		},
+		sync_install = false,
+		auto_install = false,
 
-			highlight = {
-				enable = true,
+		highlight = {
+			enable = true,
 
-				-- Setting this to true will run `:h syntax` and tree-sitter at
-				-- the same time.
-				--
-				-- Set this to `true` if you depend on 'syntax' being enabled
-				-- (like for indentation).
-				--
-				-- Using this option may slow down your editor, and you may see
-				-- some duplicate highlights.
-				--
-				-- Instead of true it can also be a list of languages.
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-		})
+			-- Setting this to true will run `:h syntax` and tree-sitter at
+			-- the same time.
+			--
+			-- Set this to `true` if you depend on 'syntax' being enabled
+			-- (like for indentation).
+			--
+			-- Using this option may slow down your editor, and you may see
+			-- some duplicate highlights.
+			--
+			-- Instead of true it can also be a list of languages.
+			additional_vim_regex_highlighting = false,
+		},
+		indent = { enable = true },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
