711d218e5ccab6c2bd79e830c35a35dd9ef9f4f9
Ashlen <dev@anthes.is>
2023-09-19 14:35:06 -0600

    refactor(nvim-undotree): real keymap moved to config

--- .config/nvim/lua/my/plugins/undotree.lua
+++ .config/nvim/lua/my/plugins/undotree.lua
@@ -2,11 +2,11 @@ return {
 	"mbbill/undotree",
 	lazy = true,
 	keys = {
-		{
-			"<leader>u",
-			function()
-				vim.cmd.UndotreeToggle()
-			end,
-		},
+		{ "<leader>u" },
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>u", function()
+			vim.cmd.UndotreeToggle()
+		end, { desc = "[U]ndo tree" })
+	end,
 }
