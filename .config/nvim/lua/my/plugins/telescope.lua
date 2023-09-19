f2bc5a70923d44887d6d2710d80583bf290c7dc9
Ashlen <dev@anthes.is>
2023-09-19 14:06:31 -0600

    fix(telescope): now works with which-key

--- .config/nvim/lua/my/plugins/telescope.lua
+++ .config/nvim/lua/my/plugins/telescope.lua
@@ -12,56 +12,43 @@ return {
 		},
 	},
 	keys = {
-		{
-			"<leader>ts",
-			function()
-				vim.cmd("Telescope")
-			end,
-			{ desc = "[T]ele[S]cope" },
-		},
-		{
-			"<leader>sf",
-			function()
-				require("telescope.builtin").find_files()
-			end,
-			{ desc = "[S]earch [F]iles" },
-		},
-		{
-			"<leader>gf",
-			function()
-				require("telescope.builtin").git_files()
-			end,
-			{ desc = "Search [G]it [F]iles" },
-		},
-		{
-			"<leader>sh",
-			function()
-				require("telescope.builtin").help_tags()
-			end,
-			{ desc = "[S]earch [H]elp" },
-		},
-		{
-			"<leader>sw",
-			function()
-				require("telescope.builtin").grep_string({
-					search = vim.fn.input("Grep > "),
-				})
-			end,
-			{ desc = "[S]earch current [W]ord" },
-		},
-		{
-			"<leader>sg",
-			function()
-				require("telescope.builtin").live_grep()
-			end,
-			{ desc = "[S]earch by [G]rep" },
-		},
-		{
-			"<leader>sd",
-			function()
-				require("telescope.builtin").diagnostics()
-			end,
-			{ desc = "[S]earch [D]iagnostics" },
-		},
+		{ "<leader>ts" },
+		{ "<leader>sf" },
+		{ "<leader>gf" },
+		{ "<leader>sh" },
+		{ "<leader>sw" },
+		{ "<leader>sg" },
+		{ "<leader>sd" },
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>ts", function()
+			vim.cmd("Telescope")
+		end, { desc = "[T]ele[S]cope" })
+
+		vim.keymap.set("n", "<leader>sf", function()
+			require("telescope.builtin").find_files()
+		end, { desc = "[S]earch [F]iles" })
+
+		vim.keymap.set("n", "<leader>gf", function()
+			require("telescope.builtin").git_files()
+		end, { desc = "Search [G]it [F]iles" })
+
+		vim.keymap.set("n", "<leader>sh", function()
+			require("telescope.builtin").help_tags()
+		end, { desc = "[S]earch [H]elp" })
+
+		vim.keymap.set("n", "<leader>sw", function()
+			require("telescope.builtin").grep_string({
+				search = vim.fn.input("Grep > "),
+			})
+		end, { desc = "[S]earch current [W]ord" })
+
+		vim.keymap.set("n", "<leader>sg", function()
+			require("telescope.builtin").live_grep()
+		end, { desc = "[S]earch by [G]rep" })
+
+		vim.keymap.set("n", "<leader>sd", function()
+			require("telescope.builtin").diagnostics()
+		end, { desc = "[S]earch [D]iagnostics" })
+	end,
 }
