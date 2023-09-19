9b59ccae0e50b8a80355a72a897508ca49591a7e
Ashlen <dev@anthes.is>
2023-09-19 12:45:20 -0600

    refactor(init.lua): organize into basic functions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,33 +1,40 @@
 -- luacheck: globals vim
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-local backup_directory = vim.env.XDG_STATE_HOME .. "/nvim/backup"
-
--- Make sure the backup directory is present and really a directory.
-if os.rename(backup_directory, backup_directory) then
-	if not os.rename(backup_directory, backup_directory .. "/") then
-		vim.api.nvim_err_writeln(backup_directory .. " is a file, not a directory.")
-		vim.opt.backup = false
+local function bootstrap_plugin_manager()
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
 	end
--- If nothing is there, attempt to create it.
-else
-	if not os.execute("mkdir -p " .. backup_directory) then
-		vim.api.nvim_err_writeln("Failed to create " .. backup_directory)
-		vim.opt.backup = false
+	vim.opt.rtp:prepend(lazypath)
+end
+
+local function setup_backup_directory()
+	local backup_directory = vim.env.XDG_STATE_HOME .. "/nvim/backup"
+
+	-- Make sure the backup directory is present and really a directory.
+	if os.rename(backup_directory, backup_directory) then
+		if not os.rename(backup_directory, backup_directory .. "/") then
+			vim.api.nvim_err_writeln(backup_directory .. " is a file, not a directory.")
+			vim.opt.backup = false
+		end
+	-- If nothing is there, attempt to create it.
+	else
+		if not os.execute("mkdir -p " .. backup_directory) then
+			vim.api.nvim_err_writeln("Failed to create " .. backup_directory)
+			vim.opt.backup = false
+		end
 	end
 end
+
+bootstrap_plugin_manager()
+setup_backup_directory()
 
 require("my.settings")
 
