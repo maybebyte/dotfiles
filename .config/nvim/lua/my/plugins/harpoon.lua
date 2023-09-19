7d930450b319b5f8c46df9eee5c1323bc25ae303
Ashlen <dev@anthes.is>
2023-09-19 13:41:04 -0600

    fix(nvim-harpoon): integrated with which-key now

--- .config/nvim/lua/my/plugins/harpoon.lua
+++ .config/nvim/lua/my/plugins/harpoon.lua
@@ -2,61 +2,46 @@ return {
 	"ThePrimeagen/harpoon",
 	lazy = true,
 	keys = {
-		{
-			"<leader>haf",
-			function()
-				require("harpoon.mark").add_file()
-			end,
-			{ desc = "[H]arpoon [a]dd [f]ile" },
-		},
-		{
-			"<leader>hqm",
-			function()
-				require("harpoon.ui").toggle_quick_menu()
-			end,
-			{ desc = "[H]arpoon [q]uick [m]enu" },
-		},
-		{
-			"<leader>h1",
-			function()
-				require("harpoon.ui").nav_file(1)
-			end,
-			{ desc = "[H]arpoon file [1]" },
-		},
-		{
-			"<leader>h2",
-			function()
-				require("harpoon.ui").nav_file(2)
-			end,
-			{ desc = "[H]arpoon file [2]" },
-		},
-		{
-			"<leader>h3",
-			function()
-				require("harpoon.ui").nav_file(3)
-			end,
-			{ desc = "[H]arpoon file [3]" },
-		},
-		{
-			"<leader>h4",
-			function()
-				require("harpoon.ui").nav_file(4)
-			end,
-			{ desc = "[H]arpoon file [4]" },
-		},
-		{
-			"<leader>hn",
-			function()
-				require("harpoon.ui").nav_next()
-			end,
-			{ desc = "[H]arpoon [n]ext" },
-		},
-		{
-			"<leader>hp",
-			function()
-				require("harpoon.ui").nav_prev()
-			end,
-			{ desc = "[H]arpoon [p]revious" },
-		},
+		{ "<leader>haf" },
+		{ "<leader>hqm" },
+		{ "<leader>h1" },
+		{ "<leader>h2" },
+		{ "<leader>h3" },
+		{ "<leader>h4" },
+		{ "<leader>hn" },
+		{ "<leader>hp" },
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>haf", function()
+			require("harpoon.mark").add_file()
+		end, { desc = "[H]arpoon [a]dd [f]ile" })
+
+		vim.keymap.set("n", "<leader>hqm", function()
+			require("harpoon.ui").toggle_quick_menu()
+		end, { desc = "[H]arpoon [q]uick [m]enu" })
+
+		vim.keymap.set("n", "<leader>h1", function()
+			require("harpoon.ui").nav_file(1)
+		end, { desc = "[H]arpoon file [1]" })
+
+		vim.keymap.set("n", "<leader>h2", function()
+			require("harpoon.ui").nav_file(2)
+		end, { desc = "[H]arpoon file [2]" })
+
+		vim.keymap.set("n", "<leader>h3", function()
+			require("harpoon.ui").nav_file(3)
+		end, { desc = "[H]arpoon file [3]" })
+
+		vim.keymap.set("n", "<leader>h4", function()
+			require("harpoon.ui").nav_file(4)
+		end, { desc = "[H]arpoon file [4]" })
+
+		vim.keymap.set("n", "<leader>hn", function()
+			require("harpoon.ui").nav_next()
+		end, { desc = "[H]arpoon [n]ext" })
+
+		vim.keymap.set("n", "<leader>hp", function()
+			require("harpoon.ui").nav_prev()
+		end, { desc = "[H]arpoon [p]revious" })
+	end,
 }
