bed083cc0c869dad8fa22526ccdca9d63407edf9
Ashlen <dev@anthes.is>
2023-09-20 14:47:36 -0600

    fix(nerdcommenter): lazy loads on keys now

--- .config/nvim/lua/my/plugins/nerdcommenter.lua
+++ .config/nvim/lua/my/plugins/nerdcommenter.lua
@@ -1,5 +1,18 @@
 return {
 	"scrooloose/nerdcommenter",
 	lazy = true,
-	event = "VeryLazy",
+	keys = {
+		{ "<leader>cc" },
+		{ "<leader>cn" },
+		{ "<leader>c<Space>" },
+		{ "<leader>cm" },
+		{ "<leader>ci" },
+		{ "<leader>cs" },
+		{ "<leader>cy" },
+		{ "<leader>c$" },
+		{ "<leader>cA" },
+		{ "<leader>ca" },
+		{ "<leader>cl" },
+		{ "<leader>cu" },
+	},
 }
