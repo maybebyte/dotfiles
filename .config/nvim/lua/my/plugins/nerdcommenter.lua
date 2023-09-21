901f44e23889febad7ae3a325a663c9576e8be36
Ashlen <dev@anthes.is>
2023-09-21 10:13:41 -0600

    Revert "fix(nerdcommenter): lazy loads on keys now"
    This reverts commit bed083cc0c869dad8fa22526ccdca9d63407edf9.
    
    Problem is that visually selecting things and then commenting no longer
    works. It acts like regular old 'c' instead.

--- .config/nvim/lua/my/plugins/nerdcommenter.lua
+++ .config/nvim/lua/my/plugins/nerdcommenter.lua
@@ -1,18 +1,5 @@
 return {
 	"scrooloose/nerdcommenter",
 	lazy = true,
-	keys = {
-		{ "<leader>cc" },
-		{ "<leader>cn" },
-		{ "<leader>c<Space>" },
-		{ "<leader>cm" },
-		{ "<leader>ci" },
-		{ "<leader>cs" },
-		{ "<leader>cy" },
-		{ "<leader>c$" },
-		{ "<leader>cA" },
-		{ "<leader>ca" },
-		{ "<leader>cl" },
-		{ "<leader>cu" },
-	},
+	event = "VeryLazy",
 }
