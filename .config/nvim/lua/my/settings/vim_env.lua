839a9cad48a8b517bc574de8894d1b19de21c43e
Ashlen <dev@anthes.is>
2023-09-27 11:49:30 -0600

    fix(vim_env): update vim.env.WEBSITE_SRC_DIR

--- .config/nvim/lua/my/settings/vim_env.lua
+++ .config/nvim/lua/my/settings/vim_env.lua
@@ -4,5 +4,5 @@ vim.env.XDG_CONFIG_HOME = vim.env.XDG_CONFIG_HOME or os.getenv("HOME") .. "/.con
 vim.env.XDG_DATA_HOME = vim.env.XDG_DATA_HOME or os.getenv("HOME") .. "/.local/share"
 vim.env.XDG_STATE_HOME = vim.env.XDG_STATE_HOME or os.getenv("HOME") .. "/.local/state"
 vim.env.WEBSITE_DOMAIN = vim.env.WEBSITE_DOMAIN or "www.anthes.is"
-vim.env.WEBSITE_SRC_DIR = vim.env.WEBSITE_SRC_DIR or os.getenv("HOME") .. "/src/website_md"
+vim.env.WEBSITE_SRC_DIR = vim.env.WEBSITE_SRC_DIR or os.getenv("HOME") .. "/src/anthesis"
 vim.env.WEBSITE_DEST_DIR = vim.env.WEBSITE_DEST_DIR or "/var/www/htdocs/" .. vim.env.WEBSITE_DOMAIN
