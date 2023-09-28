b5ac59af216b941df8bcbfa1a11546c30f38fcd1
Ashlen <dev@anthes.is>
2023-09-28 11:49:07 -0600

    feat(nvim-autocmds): highlight on yank

--- .config/nvim/lua/my/autocmds/init.lua
+++ .config/nvim/lua/my/autocmds/init.lua
@@ -1,5 +1,14 @@
 -- luacheck: globals vim
 
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		vim.opt.formatoptions:remove({ "c", "r", "o" })
