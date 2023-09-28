5355283d78fdf4ffd7e7f3802be0e95bfd30c414
Ashlen <dev@anthes.is>
2023-09-28 11:17:04 -0600

    feat(nvim-treesitter): add textobjects + binds

--- .config/nvim/lua/my/plugins/treesitter.lua
+++ .config/nvim/lua/my/plugins/treesitter.lua
@@ -2,6 +2,9 @@
 
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	lazy = true,
 	event = { "BufReadPost", "BufNewFile" },
 	build = function()
@@ -42,6 +45,58 @@ return {
 			additional_vim_regex_highlighting = false,
 		},
 		indent = { enable = true },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]a"] = "@parameter.inner",
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]A"] = "@parameter.inner",
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[a"] = "@parameter.inner",
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[A"] = "@parameter.inner",
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>lp"] = "@parameter.inner",
+					["<leader>jf"] = "@function.outer",
+					["<leader>jc"] = "@class.outer",
+				},
+				swap_previous = {
+					["<leader>hp"] = "@parameter.inner",
+					["<leader>kf"] = "@function.outer",
+					["<leader>kc"] = "@class.outer",
+				},
+			},
+		},
 	},
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
