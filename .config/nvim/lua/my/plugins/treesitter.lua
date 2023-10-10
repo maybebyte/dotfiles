def074462968c5af383055936a50a59a17de1896
Ashlen <dev@anthes.is>
2023-10-10 12:36:19 -0600

    feat(treesitter): add context plugin

--- .config/nvim/lua/my/plugins/treesitter.lua
+++ .config/nvim/lua/my/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 	lazy = true,
 	event = { "BufReadPost", "BufNewFile" },
