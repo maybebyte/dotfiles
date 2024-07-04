138d9bc85344d1162946c56eab1ac282a32e0494
Ashlen <dev@anthes.is>
2024-07-03 19:52:30 -0600

    chore(nvim): simplify parts of lsp-zero
    The curly brackets are unnecessary here, they just add visual clutter
    without a purpose. So this commit gets rid of them.

--- .config/nvim/lua/my/plugins/lsp-zero.lua
+++ .config/nvim/lua/my/plugins/lsp-zero.lua
@@ -23,16 +23,16 @@ return {
 		lazy = true,
 		event = "VeryLazy",
 		dependencies = {
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "hrsh7th/cmp-cmdline" },
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-cmdline",
 			{
 				"L3MON4D3/LuaSnip",
 				version = "2.*",
 				dependencies = {
-					{ "saadparwaiz1/cmp_luasnip" },
-					{ "rafamadriz/friendly-snippets" },
+					"saadparwaiz1/cmp_luasnip",
+					"rafamadriz/friendly-snippets",
 				},
 			},
 			{
