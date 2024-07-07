f54d8dd3a4a4c2b01ca5b7bcc267fb48eab8a90e
Ashlen <dev@anthes.is>
2024-07-07 15:31:19 -0600

    chore(nvim): remove unused plugin artifacts

--- .config/nvim/lua/my/settings/vim_g.lua
+++ .config/nvim/lua/my/settings/vim_g.lua
@@ -6,6 +6,3 @@ vim.g.mapleader = " "
 -- https://github.com/jamessan/vim-gnupg/issues/32
 vim.g.GPGUsePipes = 1
 vim.g.GPGDefaultRecipients = { "90965AE120F8E848979DEA4853670DEBCF375780" }
-
-vim.g.vimtex_view_method = "zathura"
-vim.g.markdown_folding = 1
