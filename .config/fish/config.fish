a41bbfd38951519c4b241a501e1cb7b0cc67b2b9
Ashlen <dev@anthes.is>
2024-08-25 19:44:59 -0600

    feat(fish): use vim bindings for menu selection

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,6 +37,16 @@ if status is-interactive
 	# Use vim bindings.
 	set -gx fish_key_bindings fish_vi_key_bindings
 
+	# Use vim bindings for menu selection (but use CTRL as a prefix to avoid
+	# breaking search)
+	#
+	# TODO: see if I can patch fish so that search mode automatically enables
+	# the keys to search again, and regular hjkl can be used?
+	bind -M insert \cj 'commandline --paging-mode; and down-or-search; or commandline --function execute'
+	bind -M insert \ck 'commandline --paging-mode; and up-or-search'
+	bind -M insert \ch 'commandline --paging-mode; and commandline --function backward-char; or commandline --function backward-delete-char'
+	bind -M insert \cl 'commandline --paging-mode; and commandline --function forward-char; or commandline --function clear-screen'
+
 	# Mimic neovim's cursor.
 	set -gx fish_cursor_default block
 	set -gx fish_cursor_insert line
