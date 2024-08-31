4369ccdfe7019435c8e37592acff18f4e9ba3b59
Ashlen <dev@anthes.is>
2024-08-31 16:29:25 -0600

    feat(neovim): add lazydev for neovim API stuff

--- .config/nvim/lua/my/plugins/lsp-zero.lua
+++ .config/nvim/lua/my/plugins/lsp-zero.lua
@@ -189,4 +189,20 @@ return {
 			lsp.setup()
 		end,
 	},
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		lazy = true,
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+		dependencies = {
+			{ 'Bilal2453/luvit-meta', lazy = true },
+		},
+	},
 }
