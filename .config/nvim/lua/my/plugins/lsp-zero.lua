caadd325a8014b69ae05459155d26927e4a6a255
Ashlen <dev@anthes.is>
2024-08-31 16:40:39 -0600

    fix(neovim): migrate deprecated nvim api function

--- .config/nvim/lua/my/plugins/lsp-zero.lua
+++ .config/nvim/lua/my/plugins/lsp-zero.lua
@@ -67,7 +67,7 @@ return {
 					["<C-d>"] = cmp.mapping.scroll_docs(4),
 				},
 				enabled = function()
-					return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+					return vim.api.nvim_get_option_value("buftype", { buf = 0 }) ~= "prompt"
 						or require("cmp_dap").is_dap_buffer()
 				end,
 			})
