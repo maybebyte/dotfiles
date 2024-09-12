11ea6030dbe07d95f53a06db1738579b6ccce5e5
Ashlen <dev@anthes.is>
2024-09-11 19:05:16 -0600

    feat(nvim): oil gets new bind and opts

--- .config/nvim/lua/my/plugins/oil.lua
+++ .config/nvim/lua/my/plugins/oil.lua
@@ -1,5 +1,23 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["<M-h>"] = "actions.select_split",
+				["<M-l>"] = "actions.refresh",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
+
+		-- Open parent directory in current window
+		vim.keymap.set("n", "-", function()
+			vim.cmd("Oil")
+		end, { desc = "Open parent directory" })
+	end,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
