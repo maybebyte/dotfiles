93316dc31490a71ff4917d3155d5562c120158ea
Ashlen <dev@anthes.is>
2025-04-07 15:12:02 +0200

    fix(gitsigns): check if inside a git repository

--- .config/nvim/lua/my/plugins/gitsigns.lua
+++ .config/nvim/lua/my/plugins/gitsigns.lua
@@ -37,4 +37,8 @@ return {
 			)
 		end,
 	},
+	cond = function()
+		return vim.fn.isdirectory(".git") == 1
+			or vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null") == "true\n"
+	end,
 }
