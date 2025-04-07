22c373bcb07a90ea25e449122c0fb0698480d056
Ashlen <dev@anthes.is>
2025-04-07 15:20:07 +0200

    fix(nvim): add lazy loading for oil.nvim

--- .config/nvim/lua/my/plugins/oil.lua
+++ .config/nvim/lua/my/plugins/oil.lua
@@ -1,20 +1,25 @@
 return {
 	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup({
-			columns = { "icon" },
-			keymaps = {
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-				["<M-h>"] = "actions.select_split",
-				["<M-l>"] = "actions.refresh",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-		})
+	lazy = true,
+	cmd = { "Oil" },
+	keys = {
+		{ "-", desc = "Open parent directory" },
+	},
+	opts = {
+		columns = { "icon" },
+		keymaps = {
+			["<C-h>"] = false,
+			["<C-l>"] = false,
+			["<M-h>"] = "actions.select_split",
+			["<M-l>"] = "actions.refresh",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	config = function(_, opts)
+		require("oil").setup(opts)
 
-		-- Open parent directory in current window
 		vim.keymap.set("n", "-", function()
 			vim.cmd("Oil")
 		end, { desc = "Open parent directory" })
