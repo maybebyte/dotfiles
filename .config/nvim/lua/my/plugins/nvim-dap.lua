9296897ef555ed47c99264d8e642b966643a64e7
Ashlen <dev@anthes.is>
2025-04-07 19:16:51 +0200

    refactor(nvim): new dap function for keymaps
    setup_dap_keymaps(dap): A function that sets up keymaps for debugging
    actions in Neovim using nvim-dap.

--- .config/nvim/lua/my/plugins/nvim-dap.lua
+++ .config/nvim/lua/my/plugins/nvim-dap.lua
@@ -1,5 +1,47 @@
 -- luacheck: globals vim
 
+local function setup_dap_keymaps(dap)
+	vim.keymap.set("n", "<leader>dc", function()
+		dap.continue()
+	end, { desc = "[D]ebug [C]ontinue" })
+
+	vim.keymap.set("n", "<leader>do", function()
+		dap.step_over()
+	end, { desc = "[D]ebug Step [O]ver" })
+
+	vim.keymap.set("n", "<leader>dO", function()
+		dap.step_out()
+	end, { desc = "[D]ebug Step [O]ut" })
+
+	vim.keymap.set("n", "<leader>di", function()
+		dap.step_into()
+	end, { desc = "[D]ebug Step [I]nto" })
+
+	vim.keymap.set("n", "<leader>db", function()
+		dap.toggle_breakpoint()
+	end, { desc = "[D]ebug Toggle [B]reakpoint" })
+
+	vim.keymap.set("n", "<leader>dB", function()
+		dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+	end, { desc = "[D]ebug [B]reakpoint Condition" })
+
+	vim.keymap.set("n", "<leader>dC", function()
+		dap.run_to_cursor()
+	end, { desc = "[D]ebug Run to [C]ursor" })
+
+	vim.keymap.set("n", "<leader>dj", function()
+		dap.down()
+	end, { desc = "[D]ebug Down (j)" })
+
+	vim.keymap.set("n", "<leader>dk", function()
+		dap.up()
+	end, { desc = "[D]ebug Down (k)" })
+
+	vim.keymap.set("n", "<leader>dT", function()
+		dap.terminate()
+	end, { desc = "[D]ebug [T]erminate" })
+end
+
 return {
 	"mfussenegger/nvim-dap",
 	lazy = true,
@@ -49,6 +91,7 @@ return {
 				local dap = require("dap")
 				local dapui = require("dapui")
 				dapui.setup(opts)
+
 				dap.listeners.after.event_initialized["dapui_config"] = function()
 					dapui.open({})
 				end
@@ -63,45 +106,6 @@ return {
 	},
 	config = function()
 		local dap = require("dap")
-
-		vim.keymap.set("n", "<leader>dc", function()
-			dap.continue()
-		end, { desc = "[D]ebug [C]ontinue" })
-
-		vim.keymap.set("n", "<leader>do", function()
-			dap.step_over()
-		end, { desc = "[D]ebug Step [O]ver" })
-
-		vim.keymap.set("n", "<leader>dO", function()
-			dap.step_out()
-		end, { desc = "[D]ebug Step [O]ut" })
-
-		vim.keymap.set("n", "<leader>di", function()
-			dap.step_into()
-		end, { desc = "[D]ebug Step [I]nto" })
-
-		vim.keymap.set("n", "<leader>db", function()
-			dap.toggle_breakpoint()
-		end, { desc = "[D]ebug Toggle [B]reakpoint" })
-
-		vim.keymap.set("n", "<leader>dB", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end, { desc = "[D]ebug [B]reakpoint Condition" })
-
-		vim.keymap.set("n", "<leader>dC", function()
-			dap.run_to_cursor()
-		end, { desc = "[D]ebug Run to [C]ursor" })
-
-		vim.keymap.set("n", "<leader>dj", function()
-			dap.down()
-		end, { desc = "[D]ebug Down (j)" })
-
-		vim.keymap.set("n", "<leader>dk", function()
-			dap.up()
-		end, { desc = "[D]ebug Down (k)" })
-
-		vim.keymap.set("n", "<leader>dT", function()
-			dap.terminate()
-		end, { desc = "[D]ebug [T]erminate" })
+		setup_dap_keymaps(dap)
 	end,
 }
