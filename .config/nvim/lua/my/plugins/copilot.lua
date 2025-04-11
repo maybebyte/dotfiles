c8ba9982eccda0bbda3b371bb6d02be1e7a9584f
Ashlen <dev@anthes.is>
2025-04-11 18:51:44 +0200

    feat(nvim-plugins): add CopilotChat.nvim plugin

--- .config/nvim/lua/my/plugins/copilot.lua
+++ .config/nvim/lua/my/plugins/copilot.lua
@@ -9,4 +9,42 @@ return {
 			panel = { enabled = false },
 		},
 	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		lazy = true,
+		dependencies = { "zbirenbaum/copilot.lua", "nvim-lua/plenary.nvim", branch = "master" },
+		cmd = {
+			"CopilotChat",
+			"CopilotChatOpen",
+			"CopilotChatClose",
+			"CopilotChatToggle",
+			"CopilotChatStop",
+			"CopilotChatReset",
+			"CopilotChatSave",
+			"CopilotChatLoad",
+			"CopilotChatPrompts",
+			"CopilotChatModels",
+			"CopilotChatAgents",
+			"CopilotChatCommit",
+			"CopilotChatDocs",
+			"CopilotChatExplain",
+			"CopilotChatFix",
+			"CopilotChatOptimize",
+			"CopilotChatReview",
+			"CopilotChatTests",
+		},
+		opts = {},
+		config = function(_, opts)
+			vim.opt.completeopt = vim.opt.completeopt + {
+				-- For Neovim < 0.11.0, from README
+				noinsert = true,
+				noselect = true,
+
+				-- For Neovim, even if >= 0.11.0, from README
+				popup = true
+			}
+
+			require("CopilotChat").setup(opts)
+		end,
+	},
 }
