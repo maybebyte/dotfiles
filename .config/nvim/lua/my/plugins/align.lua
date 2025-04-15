d0e8508dd2d72ca415d97a9b53fbb32cb122aef4
Ashlen <dev@anthes.is>
2025-04-15 12:50:37 +0200

    feat(nvim): vim-easy-align -> mini.align

---
+++ .config/nvim/lua/my/plugins/align.lua
@@ -0,0 +1,10 @@
+return {
+	"echasnovski/mini.align",
+	version = false,
+	lazy = true,
+	keys = {
+		{ "ga", mode = { "n", "x" }, desc = "Align text" },
+		{ "gA", mode = { "n", "x" }, desc = "Align with preview" },
+	},
+	opts = {},
+}
